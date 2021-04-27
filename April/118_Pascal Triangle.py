50677eb5d444ef577c941025c246696c4b67e9bf
Taehoon <songyi3279@gmail.com>
2021-04-27 16:08:30 -0700

    python practice apr 27

---
+++ April/118_Pascal Triangle.py
@@ -0,0 +1,10 @@
+class Solution:
+    def generate(self, numRows):
+        pascal_tri = [[1]]
+        if numRows = 1:
+            return pascal_tri
+        idx = 2
+        for idx in numRows:
+            
+
+
