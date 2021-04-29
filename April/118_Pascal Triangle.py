187edb9ea5b05ed0a1ffd3b26a1ddeabf8276916
Taehoon <songyi3279@gmail.com>
2021-04-29 15:43:56 -0700

    apr 29 practice

--- April/118_Pascal Triangle.py
+++ April/118_Pascal Triangle.py
@@ -1,10 +1,17 @@
 class Solution:
     def generate(self, numRows):
-        pascal_tri = [[1]]
-        if numRows = 1:
+        pascal_tri = [[1], [1,1]]
+        if numRows == 1:
+            return [[1]]
+        if numRows == 2:
             return pascal_tri
         idx = 2
-        for idx in numRows:
-            
-
+        for i in range(idx, numRows):
+            nextRow = [1]
+            prev_row = pascal_tri[-1]
+            for i in range(0, len(prev_row)-1):
+                nextRow.append(prev_row[i]+ prev_row[i+1])
+            nextRow.append(1)
+            pascal_tri.append(nextRow)
+        return pascal_tri
 
